4a186295f4c59c9d2dfda762ca83fdc9dce68115
nitin gautam <nkgautam1450@gmail.com>
2024-08-31 20:32:12 +0530

    first

--- android/app/src/main/kotlin/com/example/chatappflutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/chatappflutter/MainActivity.kt
@@ -2,4 +2,6 @@ package com.example.chatappflutter
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity() {
+    // You can override methods here if needed
+}
